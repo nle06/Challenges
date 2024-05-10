2a5153f0b4ed4e207f5517250ebe1f0f74278d67
nle06 <150286831+nle06@users.noreply.github.com>
2024-05-09 19:33:06 -0500

    Add files via upload

--- challenge00.rb
+++ challenge00.rb
@@ -1,30 +1,28 @@
-# Ruby program to calculate the after tax value of a specific dollar amount
-
-# Constants
+# Constants for tax rates
 GST_RATE = 0.05
 PST_RATE = 0.07
 
-# Ask the user to input the subtotal
-print "Enter the subtotal: "
+# Request user input for subtotal
+puts "Please enter the subtotal:"
 sub_total = gets.chomp.to_f
 
-# Calculate the taxes
+# Calculate tax amounts
 gst = sub_total * GST_RATE
 pst = sub_total * PST_RATE
 
-# Calculate the grand total
+# Calculate grand total
 grand_total = sub_total + gst + pst
 
-# Output the results
+# Output results
 puts "Subtotal: $#{'%.2f' % sub_total}"
-puts "PST: $#{'%.2f' % pst} - 7%"
-puts "GST: $#{'%.2f' % gst} - 5%"
+puts "PST: $#{'%.2f' % pst} - #{(PST_RATE * 100).to_i}%"
+puts "GST: $#{'%.2f' % gst} - #{(GST_RATE * 100).to_i}%"
 puts "Grand Total: $#{'%.2f' % grand_total}"
 
-# Print a message based on the grand total
-if grand_total <= 5.00
+# Determine and display the appropriate message based on the grand total
+if grand_total <= 5
   puts "Pocket Change"
-elsif grand_total > 5.00 && grand_total < 20.00
+elsif grand_total > 5 && grand_total < 20
   puts "Wallet Time"
 else
   puts "Charge It!"
